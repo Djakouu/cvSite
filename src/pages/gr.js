40525a8b4c50ac9011e2790d0e09234e14ba5699
Djakou <kimouche-mz@hotmail.fr>
2019-09-06 18:11:03 +0200

    Typos removed

--- src/pages/gr.js
+++ src/pages/gr.js
@@ -1,7 +1,7 @@
 import React from "react"
 import $ from 'jquery'
 
-import Layout from "../components/Layout";
+import Layout from "../components/layout";
 import Head from '../components/head'
 import Header from '../components/header'
 import Profile from '../components/profile'
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from "react"
 import $ from 'jquery'
 import ScriptTag from 'react-script-tag';
 
-import Layout from "../components/Layout";
+import Layout from "../components/layout";
 import Head from '../components/head'
 import Header from '../components/header'
 import Profile from '../components/profile'
