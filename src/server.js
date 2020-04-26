cbbad53b5fdcbef87e69d535ec5cf78b96769b13
Djakou <kimouche-mz@hotmail.fr>
2020-04-26 19:49:42 +0200

    Changed dist to public and added 'gatsby build': 'npm run build' to deploy to netlify

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const port = process.env.PORT || "8080";
 const app = express();
-const publicPath = path.join(__dirname, '..', 'dist');
+const publicPath = path.join(__dirname, '..', 'public');
 app.use(express.static(publicPath));// send the user to index html page inspite of the url
 app.get('*', (req, res) => {
   res.sendFile(path.join(publicPath, 'index.html'));
